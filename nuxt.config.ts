06fe6bf3a3b0f3ca520b4df1bd2301632fab43a9
hakueiMac <king20010115@gmail.com>
2024-11-05 14:38:06 +0800

    新增 Bootstrap 樣式支援，更新配置以包含全局樣式檔案

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  css: [
+    '@/assets/stylesheets/all.scss', 
+  ],
   devtools: { enabled: true }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,13 @@
 
 <template>
   <h1>Page: index</h1>
-  <p>目前路由的路徑: route.fullPath為 : {{ route.fullPath }}</p>
+  <p class="title">目前路由的路徑: route.fullPath為 : {{ route.fullPath }}</p>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
 </template>
-
-<style scoped></style>
